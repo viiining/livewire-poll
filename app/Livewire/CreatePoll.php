cd9e333ff5377bdb35dd1d381d4ad9f7d378f80a
Zinni Chang <czinni853@gmail.com>
2023-11-26 22:38:28 +0800

    [feat]: create addOption() dunction to increase the component

--- app/Livewire/CreatePoll.php
+++ app/Livewire/CreatePoll.php
@@ -7,8 +7,14 @@ use Livewire\Component;
 class CreatePoll extends Component
 {
     public $title;
+    public $options = ['First Option'];
     public function render()
     {
         return view('livewire.create-poll');
+    }
+
+    public function addOption()
+    {
+        $this->options[] = '';
     }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,7 +1,19 @@
 <div>
-	<form name="">
+	<form>
 		<label for="title">Title:</label>
 		<input type="text" id="title" wire:model.live="title"> 
 		Current title: {{ $title }}
+
+		<div class="my-5">
+			<button class="btn hover:bg-blue-400/10" wire:click.prevent="addOption">Add Option</button>
+		</div>
+
+		<div>
+			@foreach ($options as $index => $option)
+			<div class="mb-4">
+				{{ $index }}: {{ $option }}
+			</div>
+			@endforeach
+		</div>
 	</form>
 </div>
